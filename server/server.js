144a94705be4f1e6ad89c4d01d9ec0182e40040e
rbareja72 <rbareja72@gmail.com>
2018-07-19 12:56:18 +0530

    "added socket"

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+io.on('connection', (socket)=>{
+  console.log("New Connection!");
+});
+io.on('disconnect', (socket)=>{
+  console.log('Lost Connection');
+});
 
-
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`server is up on ${port}`);
 });
